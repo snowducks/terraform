cefeeda9e06c85365f647c74c2534fc0502b6417
hyunji <phj9573@naver.com>
2025-02-12 17:40:57 +0900

    feat: 보안그룹 모듈 수정

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "security_group" {
     name = var.name
-    description = security group created by terraform
+    description = "security group created by terraform"
     vpc_id = var.vpc_id
 
     dynamic "ingress" {
@@ -12,15 +12,17 @@ resource "aws_security_group" "security_group" {
           cidr_blocks = ingress.value.cidr_blocks
         }
     }
-
     dynamic "egress" {
-    content {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
+        for_each = var.egress_rules
+        content {
+          from_port   = egress.value.from_port
+          to_port     = egress.value.to_port
+          protocol    = egress.value.protocol
+          cidr_blocks = egress.value.cidr_blocks
+        }
     }
 
-  }
+
+  
 }
 
