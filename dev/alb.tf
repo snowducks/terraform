a0c4c9add3be3713173385abeac04da0a4de791d
suhoon <jobak4200nim@nate.com>
2025-02-13 15:03:27 +0900

    fix : alb 수정

--- dev/alb.tf
+++ dev/alb.tf
@@ -41,7 +41,7 @@ resource "aws_lb" "eks_alb" {
 # Target Group 생성 (EKS 서비스와 연결)
 resource "aws_lb_target_group" "eks_tg" {
   name     = "eks-target-group"
-  port     = 32000
+  port     = 80
   protocol = "HTTP"
   vpc_id   = module.vpc.vpc_id
   target_type = "instance"
