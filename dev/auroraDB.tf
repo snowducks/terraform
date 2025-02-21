b2e13b14e245a859932e558031d3cbbc0c03a625
hyunji <phj9573@naver.com>
2025-02-21 09:30:27 +0900

    feat: dev 관련 리소스 이름 환경 명시하여 변경

--- dev/auroraDB.tf
+++ dev/auroraDB.tf
@@ -1,25 +1,25 @@
-resource "aws_rds_global_cluster" "aurora_global" {
-  global_cluster_identifier = "aurora-global-cluster"
+resource "aws_rds_global_cluster" "dev_aurora_global_cluster" {
+  global_cluster_identifier = "dev-aurora-global-cluster"
   engine                    = "aurora-mysql"
-  engine_version            = "8.0.mysql_aurora.3.04.2"  # 최신 지원 버전으로 변경
+  engine_version            = "8.0.mysql_aurora.3.04.2"  # 최신 지원 버전
   database_name             = "example_db"
 }
 
-resource "aws_db_subnet_group" "aurora_subnet_group" {
-  name       = "aurora-subnet-group"
-  subnet_ids = module.vpc.private_subnets  # VPC 모듈에서 서브넷 가져오기
+resource "aws_db_subnet_group" "dev_aurora_subnet_group" {
+  name       = "dev-aurora-subnet-group"
+  subnet_ids = module.dev_vpc.private_subnets  # VPC 모듈에서 서브넷 가져오기
 
   tags = {
     Name = "AuroraSubnetGroup"
   }
 }
 
-resource "aws_rds_cluster" "aurora_primary" {
+resource "aws_rds_cluster" "dev_aurora_primary_cluster" {
 
-  engine                    = aws_rds_global_cluster.aurora_global.engine
-  engine_version            = aws_rds_global_cluster.aurora_global.engine_version
+  engine                    = aws_rds_global_cluster.dev_aurora_global_cluster.engine
+  engine_version            = aws_rds_global_cluster.dev_aurora_global_cluster.engine_version
   cluster_identifier        = "aurora-primary-cluster"
-  global_cluster_identifier = aws_rds_global_cluster.aurora_global.id
+  global_cluster_identifier = aws_rds_global_cluster.dev_aurora_global_cluster.id
   database_name             = "mydatabase"
   master_username           = var.db_master_username
   master_password           = var.db_master_password
@@ -29,31 +29,31 @@ resource "aws_rds_cluster" "aurora_primary" {
   #storage_encrypted         = true
   apply_immediately         = true
 
-  db_subnet_group_name      = aws_db_subnet_group.aurora_subnet_group.name
-  vpc_security_group_ids    = [module.aurora_sg.security_group_id]  # 모듈에서 SG 가져오기
+  db_subnet_group_name      = aws_db_subnet_group.dev_aurora_subnet_group.name
+  vpc_security_group_ids    = [module.dev_aurora_sg.security_group_id]  # 모듈에서 SG 가져오기
 
   tags = {
     Name = "AuroraPrimary"
   }
 }
 
-resource "aws_rds_cluster_instance" "aurora_primary_instance" {
+resource "aws_rds_cluster_instance" "dev_aurora_primary_instance" {
   count               = 1 
   identifier          = "aurora-primary-instance-${count.index}"
-  cluster_identifier  = aws_rds_cluster.aurora_primary.id
+  cluster_identifier  = aws_rds_cluster.dev_aurora_primary_cluster.id
   instance_class      = "db.r5.large"  
-  engine              = aws_rds_cluster.aurora_primary.engine
-  engine_version      = aws_rds_cluster.aurora_primary.engine_version
+  engine              = aws_rds_cluster.dev_aurora_primary_cluster.engine
+  engine_version      = aws_rds_cluster.dev_aurora_primary_cluster.engine_version
   publicly_accessible = false 
   apply_immediately   = true
 }
 
 
-module "aurora_sg" {
+module "dev_aurora_sg" {
   source = "../modules/security_group"
 
-  name   = "aurora-sg"
-  vpc_id = module.vpc.vpc_id
+  name   = "dev-aurora-sg"
+  vpc_id = module.dev_vpc.vpc_id
 
   ingress_rules = [
     {
@@ -75,36 +75,36 @@ module "aurora_sg" {
 }
 
 # RDS 글로벌 클러스터의 ARN 출력
-output "aurora_global_cluster_arn" {
+output "dev_aurora_global_cluster_arn" {
   description = "ARN of the Aurora Global Cluster"
-  value       = aws_rds_global_cluster.aurora_global.arn
+  value       = aws_rds_global_cluster.dev_aurora_global_cluster.arn
 }
 
 # RDS 클러스터의 엔드포인트 출력
 output "aurora_cluster_endpoint" {
   description = "Endpoint of the Aurora Cluster"
-  value       = aws_rds_cluster.aurora_primary.endpoint
+  value       = aws_rds_cluster.dev_aurora_primary_cluster.endpoint
 }
 
 # RDS 클러스터의 Reader 엔드포인트 출력
 output "aurora_cluster_reader_endpoint" {
   description = "Reader endpoint of the Aurora Cluster"
-  value       = aws_rds_cluster.aurora_primary.reader_endpoint
+  value       = aws_rds_cluster.dev_aurora_primary_cluster.reader_endpoint
 }
 
 # RDS 클러스터의 보안 그룹 ID 출력
 output "aurora_security_group_id" {
   description = "Security Group ID associated with the Aurora Cluster"
-  value       = module.aurora_sg.security_group_id
+  value       = module.dev_aurora_sg.security_group_id
 }
 
 # RDS 클러스터의 서브넷 그룹 이름 출력
 output "aurora_subnet_group_name" {
   description = "Subnet Group Name of the Aurora Cluster"
-  value       = aws_db_subnet_group.aurora_subnet_group.name
+  value       = aws_db_subnet_group.dev_aurora_subnet_group.name
 }
 
 output "aurora_global_cluster_id" {
   description = "ID of the Aurora Global Cluster"
-  value       = aws_rds_global_cluster.aurora_global.id
+  value       = aws_rds_global_cluster.dev_aurora_global_cluster.id
 }
