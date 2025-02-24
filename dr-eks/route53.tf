d52ec7664baeae3ca80b31110773315c044e8ede
suhoon <jobak4200nim@nate.com>
2025-02-24 21:43:40 +0900

    fix : output 수정

--- dr-eks/route53.tf
+++ dr-eks/route53.tf
@@ -1,30 +1,19 @@
-data "terraform_remote_state" "prod_data" {
-  backend = "s3"
 
-  config = {
-    bucket = "prod-snowduck-terraform-state"
-    key    = "prod/state-storage/terraform.tfstate"
-    region = "ap-northeast-2"
+
+resource "aws_route53_record" "prod" {
+  zone_id        = "Z10135492GRSUH9J9TX7E"
+  name           = "twinkleticket.store"
+  type           = "A"
+  set_identifier = "서울 rout53"
+
+  alias {
+    name                   = "prod-eks-alb-1407229473.ap-northeast-2.elb.amazonaws.com"
+    zone_id                = "ZWKZPGTI48KDX"
+    evaluate_target_health = true
   }
-}
 
-data "terraform_remote_state" "dr_ecs" {
-  backend = "s3"
-
-  config = {
-    bucket = "dr-ecs-snowduck-terraform-state"
-    key    = "dr-ecs/state-storage/terraform.tfstate"
-    region = "ap-southeast-1"
-  }
-}
-
-data "terraform_remote_state" "dr_eks" {
-  backend = "s3"
-
-  config = {
-    bucket = "dr-eks-snowduck-terraform-state"
-    key    = "dr-eks/state-storage/terraform.tfstate"
-    region = "ap-southeast-1"
+  weighted_routing_policy {
+    weight = 0
   }
 }
 
@@ -35,8 +24,8 @@ resource "aws_route53_record" "dr_ecs" {
   set_identifier = "dr-ecs 로드밸런서"
 
   alias {
-    name                   = data.terraform_remote_state.prod_data.outputs.alb_dns_name
-    zone_id                = data.terraform_remote_state.prod_data.outputs.alb_zone_id
+    name                   = "dr-ecs-lb-564638976.ap-southeast-1.elb.amazonaws.com"
+    zone_id                = "Z1LMS91P8CMLE5"
     evaluate_target_health = true
   }
 
@@ -46,22 +35,6 @@ resource "aws_route53_record" "dr_ecs" {
 }
 
 
-resource "aws_route53_record" "prod" {
-  zone_id        = "Z10135492GRSUH9J9TX7E"
-  name           = "twinkleticket.store"
-  type           = "A"
-  set_identifier = "서울 rout53"
-
-  alias {
-    name                   = data.terraform_remote_state.dr_ecs.outputs.dr_ecs_lb_dns_name
-    zone_id                = data.terraform_remote_state.dr_ecs.outputs.dr_ecs_lb_zone_id
-    evaluate_target_health = true
-  }
-
-  weighted_routing_policy {
-    weight = 0
-  }
-}
 
 # ✅ DR 환경 #2 (EKS, 평소 0%)
 resource "aws_route53_record" "dr_eks" {
@@ -71,8 +44,8 @@ resource "aws_route53_record" "dr_eks" {
   set_identifier = "dr-eks 로드밸런서"
 
   alias {
-    name                   = data.terraform_remote_state.dr_eks.outputs.dr_eks_alb_dns_name
-    zone_id                = data.terraform_remote_state.dr_eks.outputs.dr_eks_alb_zone_id
+    name                   = "dr-eks-alb-101966728.ap-southeast-1.elb.amazonaws.com"
+    zone_id                = "Z1LMS91P8CMLE5"
     evaluate_target_health = true
   }
 
