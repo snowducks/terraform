ae8352a7845bbfe3d2894725019443b45f2620b8
hyunji <phj9573@naver.com>
2025-02-28 09:43:47 +0900

    refactor: 프로덕션 환경 상태관리 파일 리팩토링

--- state-storage-prod/dynamodb.tf
+++ state-storage-prod/dynamodb.tf
@@ -1,4 +1,4 @@
-# Terraform 상태 잠금을 위한 DynamoDB 테이블 생성
+# 테라폼 상태 잠금을 위한 DynamoDB 테이블 생성
 resource "aws_dynamodb_table" "prod_terraform_locks" {
   name         = "prod-terraform-locks"
   billing_mode = "PAY_PER_REQUEST"
